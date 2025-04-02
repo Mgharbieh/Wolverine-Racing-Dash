b8d58beb1dbb2ce1b4bd8e13debf0891d2d5886e
Mgharbieh <mghar@umich.edu>
2025-04-02 14:06:53 -0400

    added release build

---
+++ qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/opt/Qt/6.7.3/gcc_64" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/spedhedvm/git/Instrument_Cluster_RPi/build/Desktop-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/opt/Qt/6.7.3/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
